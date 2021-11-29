d7bd74dffefc6d08d17898d4f3ba430e9fdcdf35
Jorge Molina <jorge.e.molina@hotmail.com>
2021-11-29 12:07:01 -0600

    added function to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,44 @@
-// this creates a function name "fight"
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-var playerName = window.prompt("what is your robot's name?");
-function fight() {
-    ;
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//create function
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+// Log a resulting message to the console so we know that it worked.
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack;
+// Log a resulting message to the console so we know that it worked.
+console.log(
+    enemyName + " Attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+)
+//check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
 }
-console.log("This logs a string, good for leaving yourself a message")
-//whis will do math and log 20
-console.log(10+10);
-//what is this?
-console.log("our robot's name is " + playerName)
-//fight();
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+};
+
+//executes function
+fight();
+

8fd68d547575b26b2f52443db3eba984fe4c4b65
Jorge Molina <jorge.e.molina@hotmail.com>
2021-11-29 11:09:29 -0600

    started robot gladiator

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+// this creates a function name "fight"
+
+var playerName = window.prompt("what is your robot's name?");
+function fight() {
+    ;
+}
+console.log("This logs a string, good for leaving yourself a message")
+//whis will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("our robot's name is " + playerName)
+//fight();
